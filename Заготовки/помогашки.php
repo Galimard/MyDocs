b8c39dc8dd363388a0e44828075549c02a5c766d
Maria <galimard.maria@gmail.com>
2019-10-17 09:35:18 +0800

    add

--- Заготовки/помогашки.php
+++ Заготовки/помогашки.php
@@ -7,6 +7,84 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
+        /*--------------------------------------------------общие стили--------------------------------------------------*/
+        * {
+            /*outline: 1px solid red;*/
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        html, body {
+            font-size: 16px;
+            font-family: 'ProximaNova', 'Arial', sans-serif;
+        }
+
+        body, p, h1, h2, h3, h4, h5, h6 {
+            margin: 0;
+        }
+
+        select, button {
+            border: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            outline: none;
+            background: none;
+        }
+
+        ul, li {
+            padding: 0;
+            margin: 0;
+        }
+
+        li {
+            list-style: none;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        .visually-hidden:not(:focus):not(:active),
+        input[type='checkbox'].visually-hidden,
+        input[type='radio'].visually-hidden {
+            position: absolute;
+            width: 1px;
+            height: 1px;
+            margin: -1px;
+            border: 0;
+            padding: 0;
+            white-space: nowrap;
+            -webkit-clip-path: inset(100%);
+            clip-path: inset(100%);
+            clip: rect(0 0 0 0);
+            overflow: hidden;
+        }
+
+        .template-shadow {
+            max-width: 1600px;
+            width: 100%;
+            margin: 0 auto;
+            box-shadow: 0 0 35px 3px rgba(0, 0, 0, 0.2);
+            background: url("../images/square-bg.png") center center no-repeat;
+        }
+
+        .title {
+            color: #e12457;
+            font-size: 60px;
+            font-weight: 700;
+        }
+        /*общие стили*/
+
+        @font-face {
+            font-family: 'ProximaNova';
+            src: url('../fonts/ProximaNova-Black.woff2') format('woff2');
+            src: url('../fonts/ProximaNova-Black.woff') format('woff');
+            font-weight: 900;
+            font-style: normal;
+        }
+
         /*//type number скрыть стрелки*/
         input[type='number'] {
             -moz-appearance:textfield;
@@ -97,7 +175,8 @@
             font-weight: 400;
         }
 
-        .header-form__search:focus::-webkit-input-placeholder {
+        .header-form__search:focus::-webkit-input-placeholder,
+        .cart-from__textarea:focus::-webkit-input-placeholder{
             opacity:0;
             -webkit-transition: opacity 0.5s;
             -moz-transition: opacity 0.5s;
@@ -106,7 +185,8 @@
             transition: opacity 0.5s;
         }
 
-        .header-form__search:focus:-moz-placeholder {
+        .header-form__search:focus:-moz-placeholder,
+        .cart-from__textarea:focus:-moz-placeholder {
             opacity:0;
             -webkit-transition: opacity 0.5s;
             -moz-transition: opacity 0.5s;
@@ -175,10 +255,21 @@
             cursor: pointer;
         }
 
+        /*двойной бордер*/
+        .border {
+            box-shadow:
+                    0 0 0 1px #FF4157,
+                    0 0 0 4px rgba(255,65,87,0.30);
+        }
     </style>
 </head>
 <body>
-<!---------------------------------------------------------------------------------------скрипты--------------------------------------------------------------------------------------------->
+
+<!--------------------------------------------------скрытые ссылки социалки------------------------------------------------>
+<a href="#" class="footer-bottom-socials__link footer-bottom-socials__link--vk"><span class="visually-hidden">vkontakte</span></a>
+
+
+<!--===========================================скрипты======================================================-->
 <script>
     //появление блока в зоне видимости
     function come(elem) {
@@ -344,6 +435,78 @@
          $('.mobile-search-result__item a').addClass('clearfix');
       });
    */
+</script>
+
+<script>
+    // if a user has scrolled to the bottom of the page
+    if($(window).scrollTop() == $(document).height() - $(window).height()){
+        //do something
+    }
+
+    //или
+    $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() == $(document).height()) {
+            alert("bottom!");
+        }
+    });
+</script>
+
+<script>
+    //validator
+    $('#footer-callback-form').validate({
+        submitHandler: function (form) {
+            if ($(form).valid()) {
+                $.ajax({
+                    type: $(form).attr('method'),
+                    url: '/local/templates/alans/ajax/callback.php',
+                    data: $(form).serialize(),
+                    success: function (data) {
+                        var hideSelectors = $('.form__fields, .agreement-checkbox');
+                        $.each(hideSelectors, function () {
+                            $(this).addClass('fully-hidden');
+                        });
+                        $('.thank-you').removeClass('fully-hidden');
+                    },
+                    error: function () {
+                        var hideSelectors = $('.form__fields, .agreement-checkbox');
+                        $.each(hideSelectors, function () {
+                            $(this).addClass('fully-hidden');
+                        });
+                        $('.something-wrong').removeClass('fully-hidden');
+                    }
+                })
+            }
+        },
+    });
+    jQuery.validator.addMethod("correctEmail", function (value, element) {
+        return this.optional(element) || /[a-z]+@[a-z]+\.[a-z]+/.test(value);
+    }, 'Please enter a valid email address.');
+    $.validator.addClassRules({
+        'utl': {required: true, minlength: 18},
+        'phone-input': {required: true, minlength: 18},
+        'mail-input': {email: true,},
+        'callback-form__email': {email: true,}
+    });
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "This field is required.",
+        remote: "Please fix this field.",
+        email: "Please enter a valid email address.",
+        url: "Please enter a valid URL.",
+        date: "Please enter a valid date.",
+        dateISO: "Please enter a valid date (ISO).",
+        number: "Please enter a valid number.",
+        digits: "Please enter only digits.",
+        creditcard: "Please enter a valid credit card number.",
+        equalTo: "Please enter the same value again.",
+        accept: "Please enter a value with a valid extension.",
+        maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+        minlength: jQuery.validator.format("Please enter at least {0} characters."),
+        rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+        range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+        max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+        min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+    }); //месседжи валидатора
 </script>
 
 </body>
